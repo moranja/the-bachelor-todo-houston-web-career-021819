11814a9a23d4c4c5ea2be440972f1df348ea2a4c
Adam Moran <jeffadammoran@gmail.com>
2019-02-12 18:25:15 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |bach_season, contestants|
     if bach_season == season
       contestants.each do |keys, values|
-        name_string = contestants[:"name"]
+        name_string = contestants["name"]
         if contestants[:"status"] == "Winner"
           return name_string
         end
